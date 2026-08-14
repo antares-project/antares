4407800057acb924584634411a86a41c3575bad9
Heráclito <heraclitoqsaldanha@gmail.com>
2026-08-13 23:46:34 -0300

    melhora aparencia da tela de adicionar servidor

--- client/src/lib/components/addServerModal.svelte
+++ client/src/lib/components/addServerModal.svelte
@@ -41,7 +41,7 @@
 
 <div class="fixed top-0 left-0 backdrop-blur-xs z-10 flex flex-col w-screen h-screen items-center justify-center">
     <form
-        class="relative flex flex-col items-center bg-gray-900 gap-2 p-4 border border-gray-800"
+        class="relative flex flex-col items-center bg-gray-900 gap-2 p-4 border border-gray-800 rounded-md"
         onsubmit={(event) => {
             event.preventDefault();
             submit();
@@ -53,14 +53,14 @@
             </button>
         </div>
         {#if serverInfo}
-            <div class="flex flex-col w-40 h-40 items-center justify-center">
-                <img class="flex h-full" src={`${url}/icon`} alt="icon" />
-                <h1 class="text-white h-6">{serverInfo?.title}</h1>
+            <div class="flex flex-col w-44 h-44 items-center justify-center">
+                <img class="flex rounded-2xl w-32 h-32" src={`${url}/icon`} alt="icon" />
+                <h1 class="text-white h-6 truncate">{serverInfo?.title}</h1>
             </div>
         {:else}
-            <Loading class="w-40 h-40" />
+            <Loading class="w-44 h-44" />
         {/if}
-        <input bind:value={publicKey} class="text-white p-2 rounded-md border-2 border-gray-700 bg-gray-800" placeholder="PublicKey" />
+        <input bind:value={publicKey} class="text-white p-2 rounded-md border-2 border-gray-700 bg-gray-800 text-center" placeholder="PublicKey" />
         <button type="submit" disabled={serverInfo == undefined} class="w-full bg-gray-800 text-white p-2 rounded-md cursor-pointer">Adicionar</button
         >
     </form>
